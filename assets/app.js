7dc6f5db76ffe0783b432b9799a981587042154a
dylanburkey <dylanburkey@outlook.com>
2018-06-07 18:59:54 -0400

    Updated code with correct spelling

--- assets/app.js
+++ assets/app.js
@@ -74,7 +74,7 @@ function displayGiphy() {
             });
             // Append text and images
             topicDisplay.append(p);
-            topicDisplay.append(gif);
+            topicDisplay.append(gifhyImage);
             console.log(gif);
             $("#gifs-view").append(topicDisplay);
           }
